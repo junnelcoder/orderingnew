94ba4d7a56224062f7e33f58eabfd0b0453e1c20
junnelcoder <balasabasjunnel0@gmail.com>
2024-04-22 11:02:01 +0800

    ha

--- ordering/lib/pages/config.dart
+++ ordering/lib/pages/config.dart
@@ -4,3 +4,14 @@ class AppConfig {
 
 List<Map<String, dynamic>> notes = [];
 List<int> selected = [];
+
+
+// animation-
+// new page for deleting order-
+// cart nav bar loading keyboard avoid
+// table sizes
+// 15 items per load--
+// Table toast pagselected--
+// padding sa item sa homepage di sagad
+// RESPONSIVE UI
+// DARKMODE THEME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
